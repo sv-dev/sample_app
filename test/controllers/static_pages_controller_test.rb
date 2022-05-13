6e1c0a129a649583a25b618c1f93414a095a7041
sv-dev <gertin7@gmail.com>
2022-05-13 17:44:12 +0300

    Add: Complete exercises - add new root route, tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
+  test "should get about" do
+    get root_path
+    assert_response :success
+    assert_select 'title', "About | #{@base_title}"
+  end
+
   test 'should get home' do
     get static_pages_home_url
     assert_response :success
