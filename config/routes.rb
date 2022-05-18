7b4317f5b365dea725eb5116fbfa85fc3af4f2e0
sv-dev <gertin7@gmail.com>
2022-05-18 12:22:13 +0300

    Add: Contact page with tests and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#about'
+  root 'static_pages#home'
 
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
 end
