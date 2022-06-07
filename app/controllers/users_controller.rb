21c3edd65b9303151391497348687a2c28a66ae5
sv-dev <gertin7@gmail.com>
2022-06-07 17:51:00 +0300

    Add: Requiring Logged-in Users with tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -25,7 +27,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id]) # Not the final implementation!
     if @user.update(user_params)
-      # Handle succesful update.
+      flash[:success] = 'Profile updated'
+      redirect_to @user
     else
       render 'edit'
     end
@@ -36,5 +39,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  # Before filters
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
   end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'unsuccessfull edit' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: '',
@@ -14,5 +15,22 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password_confirmation: 'bar' } }
     assert_template 'users/edit'
     assert_select 'div.alert', 'The form contains 4 errors'
+  end
+
+  test 'successful edit' do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: '',
+                                              password_confirmation: '' } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
   end
 end
