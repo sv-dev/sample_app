4af96be605381368148dfdfc7f94084a61d63628
sv-dev <gertin7@gmail.com>
2022-06-09 18:21:45 +0300

    Doc: Setting host for mail preview in browser

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_production"
 
+  host = 'arcane-river-20543.herokuapp.com' # Don't use this literally; use your local dev host instead
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
