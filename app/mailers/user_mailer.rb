4a7472f72d7333193328e78db5e99952227d6b23
sv-dev <gertin7@gmail.com>
2022-06-09 17:32:32 +0300

    Add: custom mailer view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,9 +6,8 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.account_activation.subject
   #
   def account_activation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @user = user
+    mail to: user.email, subject: 'Account activation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
