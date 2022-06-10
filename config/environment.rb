b4ab86144dfaac20862afaf1c411c56cfa967162
sv-dev <gertin7@gmail.com>
2022-06-10 19:08:19 +0300

    Revert "Fix: Setting for mailer - END - Chapter 11"
    This reverts commit c775b8db5d22dbba0e24ba6046aed2fc5864063d.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Set the default host and port to be the same as Action Mailer.
-SampleApp::Application.default_url_options = SampleApp::Application.config.action_mailer.default_url_options
